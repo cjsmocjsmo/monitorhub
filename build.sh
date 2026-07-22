1537bf70578e4c066f14b95f97193bc447c7f0aa
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2026-07-22 02:15:20 -0700

    Add build script and installation logic for ARM architectures

---
+++ build.sh
@@ -0,0 +1,2 @@
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o monitorhub64
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o monitorhub32
--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
-go build
+arch=$(uname -m)
 
 if [ -d "/usr/local/bin" ]; then
     echo "Installing monitorhub to /usr/local/bin..."
-    cp monitorhub /usr/local/bin/
+    if [ "$arch" == "aarch64" ]; then
+        cp monitorhub64 /usr/local/bin/monitorhub
+    fi
+    if [ "$arch" == "armv7l" ]; then
+        cp monitorhub32 /usr/local/bin/monitorhub
+    fi
 fi
 
 if [ -d "/etc/systemd/system"]; then
     echo "Systemd detected. Setting up service..."
     if [ ! -f "/etc/systemd/system/monitorhub.service" ]; then
-        echo "Service file does not exist. Creating..."
+        echo "Service file does not exist. Creating ..."
         cp monitorhub.service /etc/systemd/system/
     fi
 fi

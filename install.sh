6bfb9e7a0d8e988d31448c5b1eba4e397aca481d
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2026-07-22 07:34:13 -0700

    Fix syntax issues in installation script for consistency

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@ arch=$(uname -m)
 
 if [ -d "/usr/local/bin" ]; then
     echo "Installing monitorhub to /usr/local/bin..."
-    if [ "$arch" == "aarch64" ]; then
+    if [ "$arch" = "aarch64" ]; then
         cp monitorhub64 /usr/local/bin/monitorhub
     fi
-    if [ "$arch" == "armv7l" ]; then
+    if [ "$arch" = "armv7l" ]; then
         cp monitorhub32 /usr/local/bin/monitorhub
     fi
 fi
 
-if [ -d "/etc/systemd/system"]; then
+if [ -d "/etc/systemd/system" ]; then
     echo "Systemd detected. Setting up service..."
     if [ ! -f "/etc/systemd/system/monitorhub.service" ]; then
         echo "Service file does not exist. Creating ..."
